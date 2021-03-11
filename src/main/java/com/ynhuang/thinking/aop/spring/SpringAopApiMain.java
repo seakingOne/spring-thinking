0b15fa2cf1357b909cffc5b8615b9f4d09c95e41
Yunong Huang <yunong.huang@microfocus.com>
2021-03-11 14:26:20 +0800

    aop api add interceptor test.

--- src/main/java/com/ynhuang/thinking/aop/spring/SpringAopApiMain.java
+++ src/main/java/com/ynhuang/thinking/aop/spring/SpringAopApiMain.java
@@ -1,7 +1,8 @@
 package com.ynhuang.thinking.aop.spring;
 
 import com.ynhuang.thinking.aop.instances.DefaultEchaoImpl;
-import com.ynhuang.thinking.aop.instances.Echao;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.aop.MethodBeforeAdvice;
 import org.springframework.aop.framework.ProxyFactory;
 
@@ -18,8 +19,8 @@ public class SpringAopApiMain {
         //proxyFactory.setInterfaces(new Class[] {DefaultEchaoImpl.class});
         proxyFactory.setTarget(new DefaultEchaoImpl());
 
-        proxyFactory.addAdvice(new MyBeforeAdvice());
-
+        //proxyFactory.addAdvice(new MyBeforeAdvice());
+        proxyFactory.addAdvice(new MyInterceptor());
 
 
         DefaultEchaoImpl echao = (DefaultEchaoImpl)proxyFactory.getProxy();
@@ -33,6 +34,18 @@ public class SpringAopApiMain {
         @Override
         public void before(Method method, Object[] objects, Object o) throws Throwable {
             System.out.println("before log...");
+        }
+    }
+
+    static class MyInterceptor implements MethodInterceptor {
+
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+
+            System.out.println("method before invoke...");
+
+            //return null;
+            return invocation.proceed();
         }
     }
 }
